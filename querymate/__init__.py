d9ad9c3adc95caa377d0fc9030d386e9d454d3ee
Mauricio <277175+banduk@users.noreply.github.com>
2025-04-30 11:26:55 -0300

    Fix package (#21)

--- querymate/__init__.py
+++ querymate/__init__.py
@@ -2,24 +2,32 @@
 QueryMate - A Python library for building and executing database queries.
 """
 
-from querymate.core import ContainsPredicate as ContainsPredicate
-from querymate.core import EndsWithPredicate as EndsWithPredicate
-from querymate.core import EqualPredicate as EqualPredicate
-from querymate.core import FilterBuilder as FilterBuilder
-from querymate.core import (
+
+__version__ = "0.1.0"  # Update this with your actual version
+
+from .core.config import QueryMateSettings as QueryMateSettings
+from .core.config import settings as settings
+from .core.filter import ContainsPredicate as ContainsPredicate
+from .core.filter import EndsWithPredicate as EndsWithPredicate
+from .core.filter import EqualPredicate as EqualPredicate
+from .core.filter import FilterBuilder as FilterBuilder
+from .core.filter import (
     GreaterThanOrEqualPredicate as GreaterThanOrEqualPredicate,
 )
-from querymate.core import GreaterThanPredicate as GreaterThanPredicate
-from querymate.core import InPredicate as InPredicate
-from querymate.core import IsNotNullPredicate as IsNotNullPredicate
-from querymate.core import IsNullPredicate as IsNullPredicate
-from querymate.core import LessThanOrEqualPredicate as LessThanOrEqualPredicate
-from querymate.core import LessThanPredicate as LessThanPredicate
-from querymate.core import NotEqualPredicate as NotEqualPredicate
-from querymate.core import NotInPredicate as NotInPredicate
-from querymate.core import Predicate as Predicate
-from querymate.core import QueryBuilder as QueryBuilder
-from querymate.core import Querymate as Querymate
-from querymate.core import QueryMateSettings as QueryMateSettings
-from querymate.core import StartsWithPredicate as StartsWithPredicate
-from querymate.core import settings as settings
+from .core.filter import GreaterThanPredicate as GreaterThanPredicate
+from .core.filter import InPredicate as InPredicate
+from .core.filter import IsNotNullPredicate as IsNotNullPredicate
+from .core.filter import IsNullPredicate as IsNullPredicate
+from .core.filter import LessThanOrEqualPredicate as LessThanOrEqualPredicate
+from .core.filter import LessThanPredicate as LessThanPredicate
+from .core.filter import NotEqualPredicate as NotEqualPredicate
+from .core.filter import NotInPredicate as NotInPredicate
+from .core.filter import Predicate as Predicate
+from .core.filter import StartsWithPredicate as StartsWithPredicate
+from .core.query_builder import QueryBuilder as QueryBuilder
+from .core.querymate import Querymate as Querymate
+
+# Type aliases for better IDE support
+QueryMateType: type[Querymate] = Querymate
+QueryBuilderType: type[QueryBuilder] = QueryBuilder
+FilterBuilderType: type[FilterBuilder] = FilterBuilder
