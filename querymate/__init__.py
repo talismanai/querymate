1f87961d28cab55b271590e57c99c4b1a6fce2b0
Mauricio Banduk <banduk@gmail.com>
2025-04-30 11:07:54 -0300

    Fix package

--- querymate/__init__.py
+++ querymate/__init__.py
@@ -2,6 +2,22 @@
 QueryMate - A Python library for building and executing database queries.
 """
 
-from querymate.core.querymate import Querymate
-
-__all__ = ["Querymate"]
+from querymate.core import ContainsPredicate as ContainsPredicate
+from querymate.core import EndsWithPredicate as EndsWithPredicate
+from querymate.core import EqualPredicate as EqualPredicate
+from querymate.core import FilterBuilder as FilterBuilder
+from querymate.core import GreaterThanOrEqualPredicate as GreaterThanOrEqualPredicate
+from querymate.core import GreaterThanPredicate as GreaterThanPredicate
+from querymate.core import InPredicate as InPredicate
+from querymate.core import IsNotNullPredicate as IsNotNullPredicate
+from querymate.core import IsNullPredicate as IsNullPredicate
+from querymate.core import LessThanOrEqualPredicate as LessThanOrEqualPredicate
+from querymate.core import LessThanPredicate as LessThanPredicate
+from querymate.core import NotEqualPredicate as NotEqualPredicate
+from querymate.core import NotInPredicate as NotInPredicate
+from querymate.core import Predicate as Predicate
+from querymate.core import QueryBuilder as QueryBuilder
+from querymate.core import Querymate as Querymate
+from querymate.core import QueryMateSettings as QueryMateSettings
+from querymate.core import StartsWithPredicate as StartsWithPredicate
+from querymate.core import settings as settings
